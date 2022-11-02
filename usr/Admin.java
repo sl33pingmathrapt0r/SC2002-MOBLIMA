c2e3ac4513311757bad04b7573baeb7baf5eeebb
Lucas Tan <65481995+sl33pingmathrapt0r@users.noreply.github.com>
2022-11-03 01:22:51 +0800

    Add files via upload

---
+++ usr/Admin.java
@@ -0,0 +1,57 @@
+package usr;
+
+import java.util.*;
+
+class Admin extends User {
+    private boolean isAdmin= true;
+    private String username;
+    private String pw;
+
+    private static Scanner scan= new Scanner(System.in);
+
+    Admin(String username, String pw) {
+        this.username= username;
+        this.pw= pw;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public String getUser() {
+        return username;
+    }
+
+    boolean checkPW(String pw) {
+        return (this.pw.equals(pw)) ? true : false;
+    }
+
+    // TODO
+    public void createMovieListing() {
+
+    }
+
+    public void updateMovieListing() {
+
+    }
+
+    public void deleteMovieListing() {
+
+    }
+
+    public void createCinemaShowtimes() {
+
+    }
+
+    public void updateCinemaListing() {
+
+    }
+
+    public void deleteCinemaListing() {
+
+    }
+
+    public void configureSystemSettings() {
+        // ???
+    }
+}
---
+++ usr/MovieGoer.java
@@ -0,0 +1,59 @@
+package usr;
+
+// import tickets.*;
+
+public class MovieGoer extends User {
+    private String username;
+    private String pw;
+
+    private String[] particulars= new String[3];
+    // {name, mobile number, email address}
+    // private ArrayList<Ticket> bookingHistory= new ArrayList<Ticket>();
+
+
+    MovieGoer(String username, String pw, String name, String hp, String email) {
+        this.username= username;
+        this.pw= pw;
+        particulars[0]= name;
+        particulars[1]= hp;
+        particulars[2]= email;
+    }
+
+
+    public String getUser() {
+        return username;
+    }
+
+    boolean checkPW(String pw) {
+        return (this.pw.equals(pw)) ? true : false;
+    }
+
+    // TODO
+    // public void viewMovieDetails(Movie movie) {
+
+    // }
+
+    public void selectSeat() {
+
+    }
+
+    public void bookTicket() {
+
+    }
+
+    // public void viewBookingHistory() {
+    //     for (int i=0; i<bookingHistory.size(); i++) {
+    //         System.out.println( 
+    //             (i+1) + ":\n" + 
+    //             "\t" + bookingHistory.get(i).getCinema() + "\n" +
+    //             "\t" + bookingHistory.get(i).getMovie() + "\n" +
+    //             "\t" + bookingHistory.get(i).getTime() + "\n" +
+    //             "\t" + bookingHistory.get(i).getSeat() + "\n"
+    //             );
+    //     }
+    // }
+
+    public void listTop5Movies() {
+        
+    }
+}
