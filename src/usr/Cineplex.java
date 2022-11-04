232fbad9c6d2f5f7b4a45b501a2cd34b9c527fc6
moroha29 <junhong.tan@gmail.com>
2022-11-04 23:42:00 +0800

    draft 1
    i have no idea what im doing

--- src/usr/Cineplex.java
+++ src/usr/Cineplex.java
@@ -1,6 +1,48 @@
-package src.usr;
-package Group_Assignment;
+package Cineplex;
+package cinema;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Cineplex {
-    public
+    public static int MovieListCounter=0;
+    final private static String path = Path.of("").toAbsolutePath().toString()
+
+    public Cineplex(){
+		boolean b;
+		File f = new File(this.path+"MovieList");
+		if (!f.exists()){
+			b = f.mkdir();
+        }
+	}
+
+    public void AddMovie(String MovieName,String Type,String MovieRating,String ShowingStatus) throws IOException {
+		File g = new File(this.path+"MovieList\\"+MovieName+"@.txt");
+		if (g.exists()) {
+			System.out.println("Movie already exists");
+			return;
+		}
+		FileWriter w = new FileWriter(this.path+"MovieList\\"+MovieName+"@.txt",true);
+		w.append( Type + "\n" + MovieRating + "\n" + ShowingStatus + "\n");
+		w.close();
+        this.MovieListCounter++;
+	}
+
+    public void RemoveMovie(String MovieName) throws IOException {
+		boolean b;
+        File g = new File(this.path+"MovieList\\"+MovieName+"@"+"@.txt");
+		if (g.exists()) {
+            b=g.delete();
+            this.MovieListCounter--;
+			System.out.println("Movie has been deleted.");
+            return;
+		}
+    }
+
+    public void ListMovie() throws IOException {
+        File[] movFolder = new File(this.path+"MovieList").listFiles();
+        for(File it : movFolder) System.out.println(it.getName());
+    }
 }
