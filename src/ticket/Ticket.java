527fa743337c3ba30776c7d7095041112bb9f70f
PockXuan <khorpockxuan@gmail.com>
2022-11-05 15:00:52 +0800

    Changed calculate price method to class method instead

--- src/ticket/Ticket.java
+++ src/ticket/Ticket.java
@@ -2,6 +2,7 @@ package src.ticket;
 
 public class Ticket {
 
+	private String movieTitle;
 	private TypeOfMovie typeOfMovie;
 	private ClassOfCinema classOfCinema;
 	private String clientName;
@@ -10,6 +11,7 @@ public class Ticket {
 	private int[] timeOfMovie; //2-tuple of int (hour,min) in a 24H system
 	private AgeGroup ageGroup;
 	private double price;
+	private String seatID;
 
 	/**
 	 * Constructor class for ticket
@@ -22,9 +24,10 @@ public class Ticket {
 	 * @param ageGroup
 	 * @param price
 	 */
-	public Ticket(TypeOfMovie typeOfMovie, ClassOfCinema classOfCinema, String clientName, String clientContact, Day dayOfWeek,
-			int[] timeOfMovie, AgeGroup ageGroup) {
+	public Ticket(String movieTitle,TypeOfMovie typeOfMovie, ClassOfCinema classOfCinema, String clientName, String clientContact, Day dayOfWeek,
+			int[] timeOfMovie, AgeGroup ageGroup, String seatID) {
 		
+		this.movieTitle=movieTitle;
 		this.typeOfMovie = typeOfMovie;
 		this.classOfCinema = classOfCinema;
 		this.clientName = clientName;
@@ -32,11 +35,12 @@ public class Ticket {
 		this.dayOfWeek = dayOfWeek;
 		this.timeOfMovie = timeOfMovie;
 		this.ageGroup = ageGroup;
-		this.price=this.calculatePrice();
+		this.seatID=seatID;
+		this.price=Ticket.calculatePrice(classOfCinema, typeOfMovie, ageGroup, dayOfWeek, timeOfMovie);
 		
 	}
-	/*
-	 * Function to calculate price of ticket based on class of cinema and type of movie
+	/**
+	 * Functions to calculate price of ticket based on class of cinema and type of movie
 	 * For Cinema Class there is Regular, Atmos and Platinum
 	 * For Type of movie there is regular/ digital, block buster and 3D
 	 * There are no 3D movie for Atmos and Platinum class
@@ -45,53 +49,63 @@ public class Ticket {
 	 * Remaining prices that are not reflected in the website such as 3D movies in general, platinum weekends are just a rough guess
 	 */
 	
-	private double calculatePrice() {
+	/**
+	 * Class method to check price of ticket without creating the object
+	 * @param classOfCinema
+	 * @param typeOfMovie
+	 * @param ageGroup
+	 * @param dayOfWeek
+	 * @param timeOfMovie
+	 * @return price of ticket
+	 */
+	private static double calculatePrice(ClassOfCinema classOfCinema,TypeOfMovie typeOfMovie,AgeGroup ageGroup,Day dayOfWeek, int[] timeOfMovie){
+		
 
 		//Regular Cinema
-		if(this.classOfCinema==ClassOfCinema.REGULAR) {
-			switch(this.dayOfWeek) {
+		if(classOfCinema==ClassOfCinema.REGULAR) {
+			switch(dayOfWeek) {
 			
 			case THURSDAY:
 				//student
-				if(this.ageGroup==AgeGroup.STUDENT && this.timeOfMovie[0]<18) {
+				if(ageGroup==AgeGroup.STUDENT && timeOfMovie[0]<18) {
 					//3D movie
-					if(this.typeOfMovie==TypeOfMovie.D3) return 9.00;
+					if(typeOfMovie==TypeOfMovie.D3) return 9.00;
 					//regular and digital are $1 cheaper than blockbuster 
 					else {
-						if(this.typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 8.00;
+						if(typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 8.00;
 						else return 7.00;
 					}
 				}
 				//senior citizen
-				if(this.ageGroup==AgeGroup.SENIOR && this.timeOfMovie[0]<18 && this.classOfCinema==ClassOfCinema.REGULAR)	return 5.00;
+				if(ageGroup==AgeGroup.SENIOR && timeOfMovie[0]<18 && classOfCinema==ClassOfCinema.REGULAR)	return 5.00;
 				//adult
-				if(this.typeOfMovie==TypeOfMovie.D3) 	return 12.00;
+				if(typeOfMovie==TypeOfMovie.D3) 	return 12.00;
 				else {
-					if(this.typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 11.00;
+					if(typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 11.00;
 					else return 10.00;
 				}
 
 			case FRIDAY:
 				//student
-				if(this.ageGroup==AgeGroup.STUDENT && this.timeOfMovie[0]<18) {
-					if(this.typeOfMovie==TypeOfMovie.D3) return 9.00;
+				if(ageGroup==AgeGroup.STUDENT && timeOfMovie[0]<18) {
+					if(typeOfMovie==TypeOfMovie.D3) return 9.00;
 					else {
-						if(this.typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 8.00;
+						if(typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 8.00;
 						else return 7.00;
 					}
 				}
 				//senior citizen
-				if(this.ageGroup==AgeGroup.SENIOR && this.timeOfMovie[0]<18 && this.classOfCinema==ClassOfCinema.REGULAR)	return 5.00;
+				if(ageGroup==AgeGroup.SENIOR && timeOfMovie[0]<18 && classOfCinema==ClassOfCinema.REGULAR)	return 5.00;
 				//3D movies on Fridays are regardless of timing
-				if(this.typeOfMovie==TypeOfMovie.D3) {
+				if(typeOfMovie==TypeOfMovie.D3) {
 					return 18.00;
 				}
 				else {
 					//before 6pm regular & digital
-					if(this.timeOfMovie[0]<18) return 10.00;
+					if(timeOfMovie[0]<18) return 10.00;
 					//after 6pm regular & digital
 					else {
-						if(this.typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 15.50;
+						if(typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 15.50;
 						else return 14.50;
 					}
 				}
@@ -101,74 +115,72 @@ public class Ticket {
 			case SATURDAY:
 			case SUNDAY:
 				//3D movies
-				if(this.typeOfMovie==TypeOfMovie.D3) 	return 18.00;
+				if(typeOfMovie==TypeOfMovie.D3) 	return 18.00;
 				//regular and digital are $1 cheaper than blockbuster 
 				else {
-					if(this.typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 15.50;
+					if(typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 15.50;
 					else return 14.50;
 				}
 
 				// Monday, Tuesday, Wednesday
 			default:
 				//student
-				if(this.ageGroup==AgeGroup.STUDENT && this.timeOfMovie[0]<18) {
-					if(this.typeOfMovie==TypeOfMovie.D3) return 9.00;
+				if(ageGroup==AgeGroup.STUDENT && timeOfMovie[0]<18) {
+					if(typeOfMovie==TypeOfMovie.D3) return 9.00;
 					else {
-						if(this.typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 8.00;
+						if(typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 8.00;
 						else return 7.00;
 					}
 				}
 				//senior citizen
-				if(this.ageGroup==AgeGroup.SENIOR && this.timeOfMovie[0]<18 && this.classOfCinema==ClassOfCinema.REGULAR)	return 5.00;
+				if(ageGroup==AgeGroup.SENIOR && timeOfMovie[0]<18 && classOfCinema==ClassOfCinema.REGULAR)	return 5.00;
 				//adult
-				if(this.typeOfMovie==TypeOfMovie.D3) 	return 12.00;
+				if(typeOfMovie==TypeOfMovie.D3) 	return 12.00;
 				else {
-					if(this.typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 11.00;
+					if(typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 11.00;
 					else return 10.00;
 				}
 
 			}
 		}
 		//Platinum Class Cinema
-		else if(this.classOfCinema==ClassOfCinema.PLATINUM) {
-			switch(this.dayOfWeek) {
+		else if(classOfCinema==ClassOfCinema.PLATINUM) {
+			switch(dayOfWeek) {
 				case FRIDAY:
 				case SATURDAY:
 				case SUNDAY:
 					//no student/ senior citizen promo
-					if(this.typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 38.00;
+					if(typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 38.00;
 					else return 35.00;
 				default:
-					if(this.ageGroup==AgeGroup.STUDENT) return 16.00;
-					if(this.typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 28.00;
+					if(ageGroup==AgeGroup.STUDENT) return 16.00;
+					if(typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 28.00;
 					else return 25.00;
 			}
 		}
 		//Atmos class
 		else {
-			switch(this.dayOfWeek) {
+			switch(dayOfWeek) {
 			case FRIDAY:
 			case SATURDAY:
 			case SUNDAY:
-				if(this.typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 17.00;
+				if(typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 17.00;
 				else return 15.50;
 				
 			default:
-				if(this.typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 15.50;
+				if(typeOfMovie==TypeOfMovie.BLOCKBUSTER) return 15.50;
 				else return 14.00;
 		}
 		}
 	}
 
-	public int[] getTimeOfMovie() {
-		return timeOfMovie;
+	public String getMovieTitle() {
+		return movieTitle;
 	}
 
-
-	public void setTimeOfMovie(int[] timeOfMovie) {
-		this.timeOfMovie = timeOfMovie;
+	public void setMovieTitle(String movieTitle) {
+		this.movieTitle = movieTitle;
 	}
-
 
 	public TypeOfMovie getTypeOfMovie() {
 		return typeOfMovie;
@@ -210,14 +222,36 @@ public class Ticket {
 		this.dayOfWeek = dayOfWeek;
 	}
 
-	public double getPrice() {
-		return price;
+	public int[] getTimeOfMovie() {
+		return timeOfMovie;
 	}
+
+	public void setTimeOfMovie(int[] timeOfMovie) {
+		this.timeOfMovie = timeOfMovie;
+	}
+
 	public AgeGroup getAgeGroup() {
 		return ageGroup;
 	}
+
 	public void setAgeGroup(AgeGroup ageGroup) {
 		this.ageGroup = ageGroup;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getSeatID() {
+		return seatID;
+	}
+
+	public void setSeatID(String seatID) {
+		this.seatID = seatID;
 	}
 
 
