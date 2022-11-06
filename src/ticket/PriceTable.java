f7e93bb8c8aa57dd08cc31c21773175d4b19c448
PockXuan <khorpockxuan@gmail.com>
2022-11-06 16:14:42 +0800

    made a fking price table

--- src/ticket/PriceTable.java
+++ src/ticket/PriceTable.java
@@ -1,45 +1,163 @@
 package src.ticket;
+
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
-public class PriceTable{
+public class PriceTable {
 
-    private ClassOfCinema j;
-    private Hashtable<ClassOfCinema, 
-            Hashtable<AgeGroup, 
-            Hashtable<TypeOfMovie, 
-            Hashtable<Day,Double> > > > priceTable;
+    private Path currentRelativePath = Paths.get("");
+    private String s = currentRelativePath.toAbsolutePath().toString();
+    File rPT = new File(s + "/src/ticket/regularPriceTable");
+    File pPT = new File(s + "/src/ticket/platinumPriceTable");
+    File aPT = new File(s + "/src/ticket/atmosPriceTable");
+    private Hashtable<Day, Hashtable<AgeGroup, Hashtable<TypeOfMovie, Double>>> regularPriceTable;
+    private Hashtable<Day, Hashtable<AgeGroup, Hashtable<TypeOfMovie, Double>>> platinumPriceTable;
+    private Hashtable<Day, Hashtable<AgeGroup, Hashtable<TypeOfMovie, Double>>> atmosPriceTable;
 
-    public PriceTable(){
-        
-        priceTable = new Hashtable<>();
-        for(ClassOfCinema i : ClassOfCinema.values()){
-            priceTable.put(i,new Hashtable<>());
-            for(AgeGroup j : AgeGroup.values()){
-                priceTable.get(i).put(j,new Hashtable<>());
-                for(TypeOfMovie k: TypeOfMovie.values()){
-                    priceTable.get(i).get(j).put(k,new Hashtable<>());
-                    for(Day l: Day.values()){
-                        priceTable.get(i).get(j).get(k).put(l,0.0);
+    public PriceTable() {
+
+        this.regularPriceTable = new Hashtable<>();
+        this.fileToTable(this.regularPriceTable, this.rPT);
+
+        this.platinumPriceTable = new Hashtable<>();
+        this.fileToTable(this.platinumPriceTable, this.pPT);
+
+        this.atmosPriceTable = new Hashtable<>();
+        this.fileToTable(this.atmosPriceTable, this.aPT);
+    }
+
+    private void fileToTable(Hashtable<Day, Hashtable<AgeGroup, Hashtable<TypeOfMovie, Double>>> table, File file) {
+        try {
+            Scanner fr = new Scanner(file);
+            while (fr.hasNextLine()) {
+                for (Day i : Day.values()) {
+                    String temp = fr.nextLine();
+                    Day day = Day.valueOf(temp);
+
+                    table.put(day, new Hashtable<>());
+                    for (AgeGroup j : AgeGroup.values()) {
+                        String temp2 = fr.nextLine();
+                        AgeGroup age = AgeGroup.valueOf(temp2);
+
+                        table.get(day).put(age, new Hashtable<>());
+                        for (TypeOfMovie k : TypeOfMovie.values()) {
+                            String temp3 = fr.nextLine();
+                            TypeOfMovie type = TypeOfMovie.valueOf(temp3);
+                            String l = fr.nextLine();
+                            Double price = Double.valueOf(l);
+                            fr.nextLine();
+                            table.get(day).get(age).put(type, price);
+
+                        }
                     }
+
                 }
             }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 
-    public static void main(String[] args){
+    private void saveTable(Hashtable<Day, Hashtable<AgeGroup, Hashtable<TypeOfMovie, Double>>> table, File file){
+        try {
+            FileWriter fw = new FileWriter(rPT);
+            for (Object i : this.regularPriceTable.keySet()) {
+                fw.append(i + "\n");
+                for (Object j : regularPriceTable.get(i).keySet()) {
+                    fw.append(j + "\n");
+                    for (Map.Entry<TypeOfMovie, Double> k : regularPriceTable.get(i).get(j).entrySet()) {
+                        fw.append(k.getKey() + "\n");
+                        fw.append(k.getValue() + "\n");
+                        fw.append("\n");
+                    }
+                }
+            }
+            fw.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public boolean setPrice(ClassOfCinema classOfCinema,Day day, AgeGroup ageGroup, TypeOfMovie typeOfMovie, double price){
+        try {
+            switch (classOfCinema){
+                case REGULAR:
+                    this.regularPriceTable.get(day).get(ageGroup).put(typeOfMovie,price);
+                    break;
+                case PLATINUM:
+                    this.platinumPriceTable.get(day).get(ageGroup).put(typeOfMovie,price);
+                    break;
+                case ATMOS:
+                    this.atmosPriceTable.get(day).get(ageGroup).put(typeOfMovie,price);
+                    break;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+            return true;
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public double checkPrice(ClassOfCinema classOfCinema,Day day, AgeGroup ageGroup, TypeOfMovie typeOfMovie){
+        double price=0;
+        switch (classOfCinema){
+            case REGULAR:
+                price=this.regularPriceTable.get(day).get(ageGroup).get(typeOfMovie);
+                break;
+            case PLATINUM:
+                price=this.platinumPriceTable.get(day).get(ageGroup).get(typeOfMovie);
+                break;
+            case ATMOS:
+                price=this.atmosPriceTable.get(day).get(ageGroup).get(typeOfMovie);
+                break;
+            default:
+                System.out.println("Invalid option");
+                break;
+        }
+        return price;
+    }
+    public void saveAllPriceTable() {
+        
+        saveTable(this.regularPriceTable,this.rPT);
+        saveTable(this.platinumPriceTable, this.pPT);
+        saveTable(this.atmosPriceTable, this.aPT);
+    }
+
+    //for me to test stuff cuz mans lazy to make a test app
+    public static void main(String[] args) {
 
         PriceTable p = new PriceTable();
-        for(Object i : p.priceTable.keySet()){
-            System.out.println(i);
-            for(Object j :p.priceTable.get(i).keySet()){
-                System.out.println(j);
-                for(Object k:p.priceTable.get(i).get(j).entrySet()){
-                    System.out.println(k);
-                }
-                System.out.println();
-            }
-            System.out.println();
-        }
+        System.out.println(p.regularPriceTable);
+        System.out.println();
+        System.out.println(p.platinumPriceTable);
+        System.out.println();
+        System.out.println(p.atmosPriceTable);
+
+        /*
+         * PriceTable p = new PriceTable();
+         * for(Object i : p.regularPriceTable.keySet()){
+         *      for(Object j :p.regularPriceTable.get(i).keySet()){
+         *          for(Object k:p.regularPriceTable.get(i).get(j).keySet()){
+         *              for(Map.Entry<Day,Double> l:p.regularPriceTable.get(i).get(j).get(k).entrySet()){
+         *                  System.out.println(i);
+         *                  System.out.println(j);
+         *                  System.out.println(k);
+         *                  System.out.println(l.getKey());
+         *                  System.out.println(l.getValue());
+         *                  System.out.println();
+         *              }
+         *          }
+         *      }
+         * }
+         */
     }
 }
--- src/ticket/Ticket.java
+++ src/ticket/Ticket.java
@@ -12,7 +12,8 @@ public class Ticket {
 	private AgeGroup ageGroup;
 	private double price;
 	private String seatID;
-
+	private PriceTable priceTable=new PriceTable();
+	
 	/**
 	 * Constructor class for ticket
 	 * 
@@ -38,8 +39,8 @@ public class Ticket {
 		this.timeOfMovie = timeOfMovie;
 		this.ageGroup = ageGroup;
 		this.seatID = seatID;
-		this.price = Ticket.calculatePrice(this.classOfCinema, this.typeOfMovie, this.ageGroup, this.dayOfWeek,
-				this.timeOfMovie);
+		this.priceTable = new PriceTable();
+		this.price = priceTable.checkPrice(classOfCinema, dayOfWeek, ageGroup, typeOfMovie);
 
 	}
 
@@ -65,151 +66,50 @@ public class Ticket {
 	 * @param timeOfMovie
 	 * @return price of ticket
 	 */
-	private static double calculatePrice(ClassOfCinema classOfCinema, TypeOfMovie typeOfMovie, AgeGroup ageGroup,
+	public double calculatePrice(ClassOfCinema classOfCinema, TypeOfMovie typeOfMovie, AgeGroup ageGroup,
 			Day dayOfWeek, int[] timeOfMovie) {
-
-		// Regular Cinema
-		if (classOfCinema == ClassOfCinema.REGULAR) {
-			switch (dayOfWeek) {
-
-				case THURSDAY:
-					// student
-					if (ageGroup == AgeGroup.STUDENT && timeOfMovie[0] < 18) {
-						// 3D movie
-						if (typeOfMovie == TypeOfMovie.D3)
-							return 9.00;
-						// regular and digital are $1 cheaper than blockbuster
-						else {
-							if (typeOfMovie == TypeOfMovie.BLOCKBUSTER)
-								return 8.00;
-							else
-								return 7.00;
-						}
-					}
-					// senior citizen
-					if (ageGroup == AgeGroup.SENIOR && timeOfMovie[0] < 18 && classOfCinema == ClassOfCinema.REGULAR)
-						return 5.00;
-					// adult
-					if (typeOfMovie == TypeOfMovie.D3)
-						return 12.00;
-					else {
-						if (typeOfMovie == TypeOfMovie.BLOCKBUSTER)
-							return 11.00;
-						else
-							return 10.00;
-					}
-
-				case FRIDAY:
-					// student
-					if (ageGroup == AgeGroup.STUDENT && timeOfMovie[0] < 18) {
-						if (typeOfMovie == TypeOfMovie.D3)
-							return 9.00;
-						else {
-							if (typeOfMovie == TypeOfMovie.BLOCKBUSTER)
-								return 8.00;
-							else
-								return 7.00;
-						}
-					}
-					// senior citizen
-					if (ageGroup == AgeGroup.SENIOR && timeOfMovie[0] < 18 && classOfCinema == ClassOfCinema.REGULAR)
-						return 5.00;
-					// 3D movies on Fridays are regardless of timing
-					if (typeOfMovie == TypeOfMovie.D3) {
-						return 18.00;
-					} else {
-						// before 6pm regular & digital
-						if (timeOfMovie[0] < 18)
-							return 10.00;
-						// after 6pm regular & digital
-						else {
-							if (typeOfMovie == TypeOfMovie.BLOCKBUSTER)
-								return 15.50;
-							else
-								return 14.50;
-						}
-					}
-
-					// weekends and public holiday
-				case PH:
-				case SATURDAY:
-				case SUNDAY:
-					// 3D movies
-					if (typeOfMovie == TypeOfMovie.D3)
-						return 18.00;
-					// regular and digital are $1 cheaper than blockbuster
-					else {
-						if (typeOfMovie == TypeOfMovie.BLOCKBUSTER)
-							return 15.50;
-						else
-							return 14.50;
-					}
-
-					// Monday, Tuesday, Wednesday
-				default:
-					// student
-					if (ageGroup == AgeGroup.STUDENT && timeOfMovie[0] < 18) {
-						if (typeOfMovie == TypeOfMovie.D3)
-							return 9.00;
-						else {
-							if (typeOfMovie == TypeOfMovie.BLOCKBUSTER)
-								return 8.00;
-							else
-								return 7.00;
-						}
-					}
-					// senior citizen
-					if (ageGroup == AgeGroup.SENIOR && timeOfMovie[0] < 18 && classOfCinema == ClassOfCinema.REGULAR)
-						return 5.00;
-					// adult
-					if (typeOfMovie == TypeOfMovie.D3)
-						return 12.00;
-					else {
-						if (typeOfMovie == TypeOfMovie.BLOCKBUSTER)
-							return 11.00;
-						else
-							return 10.00;
-					}
-
-			}
+			
+	/**
+	 * Regular cinema
+	 * Mon-Thurs, Fri before 6
+	 * 9 Student 3D 
+	 * 8 Student Blockuster 
+	 * 7 Student Digital  
+	 * 5 Senior Digital 
+	 * 12 Adult 3D
+	 * 11 Adult Blockbuster
+	 * 10 Adult Digital
+	 * 
+	 * Fri after 6, Weekends & PH
+	 * 18 Adult 3D
+	 * 15.50 Adult Blockbuster
+	 * 14.50 Adult Digital
+	 * 
+	 * Platinum Cinema
+	 * Weekends & PH
+	 * 38 Blockbuster
+	 * 35 Digital
+	 * 
+	 * Weekdays
+	 * 16 Student
+	 * 28 Adult Blockbuster
+	 * 25 Adult Digital
+	 * 
+	 * Atmos Cinema
+	 * Weekends & PH
+	 * 17 Blockbuster
+	 * 15.50 Digital
+	 * 
+	 * Weekday
+	 * 15.50 Blockbuster
+	 * 14 Digital
+	 */
+		//lazy man method to convert fri evening to weekend price pog
+		if(dayOfWeek==Day.FRIDAY && timeOfMovie[0]>18){
+			return this.priceTable.checkPrice(classOfCinema, Day.SATURDAY, ageGroup, typeOfMovie);
 		}
-		// Platinum Class Cinema
-		else if (classOfCinema == ClassOfCinema.PLATINUM) {
-			switch (dayOfWeek) {
-				case FRIDAY:
-				case SATURDAY:
-				case SUNDAY:
-					// no student/ senior citizen promo
-					if (typeOfMovie == TypeOfMovie.BLOCKBUSTER)
-						return 38.00;
-					else
-						return 35.00;
-				default:
-					if (ageGroup == AgeGroup.STUDENT)
-						return 16.00;
-					if (typeOfMovie == TypeOfMovie.BLOCKBUSTER)
-						return 28.00;
-					else
-						return 25.00;
-			}
-		}
-		// Atmos class
-		else {
-			switch (dayOfWeek) {
-				case FRIDAY:
-				case SATURDAY:
-				case SUNDAY:
-					if (typeOfMovie == TypeOfMovie.BLOCKBUSTER)
-						return 17.00;
-					else
-						return 15.50;
-
-				default:
-					if (typeOfMovie == TypeOfMovie.BLOCKBUSTER)
-						return 15.50;
-					else
-						return 14.00;
-			}
+		else{
+			return this.priceTable.checkPrice(classOfCinema, dayOfWeek, ageGroup, typeOfMovie);
 		}
 	}
 
