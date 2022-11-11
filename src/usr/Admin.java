7e789be8981bae1a182a0eb02b6a8813d340a7bf
macsdude <91863775+macsdude@users.noreply.github.com>
2022-11-12 00:31:04 +0800

    can update delete and add movie listings now

--- src/usr/Admin.java
+++ src/usr/Admin.java
@@ -1,5 +1,6 @@
 package usr;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 // import MovieList.*;
@@ -71,11 +72,11 @@ class Admin extends User {
         * the target movie then create another
         * updates one of the 4 things in movie
         */
-        ArrayList <Movie> movieList = cineplex.getMovieList();
+        ArrayList <Movie> movieList = cineplex.getListOfMovies();
         System.out.println("Which movie would you like to update? ");
-        for(int i=0;i<movieList.size();i++) System.out.println(i+". " +movieList.get(i).getTitle());
-        System.out.println(movieList.size() + ". Exit");
-        int choice = InputHandling.getInt("", "Invalid input: ", 0, movieList.size());
+        for(int i=0;i<movieList.size();i++) System.out.println((i+1)+". " +movieList.get(i).getTitle());
+        System.out.println((movieList.size()+1) + ". Exit");
+        int choice = InputHandling.getInt("", "Invalid input: ", 1, movieList.size()+1)-1;
         if(choice==movieList.size()) return;
         String title = movieList.get(choice).getTitle();
         MovieList.updateMovieAdmin(title);
@@ -83,48 +84,81 @@ class Admin extends User {
     }
 
     public void deleteMovieListing(Cineplex cineplex) {
-        ArrayList <Movie> movieList = cineplex.getMovieList();
+        ArrayList <Movie> movieList = cineplex.getListOfMovies();
         System.out.println("Which movie would you like to delete? ");
-        for(int i=0;i<movieList.size();i++) System.out.println(i+". " +movieList.get(i).getTitle());
-        System.out.println(movieList.size() + ". Exit");
-        int choice = InputHandling.getInt("", "Invalid input: ", 0, movieList.size());
+        for(int i=0;i<movieList.size();i++) System.out.println((i+1)+". " +movieList.get(i).getTitle());
+        System.out.println((movieList.size()+1) + ". Exit");
+        int choice = InputHandling.getInt("", "Invalid input: ", 1, movieList.size()+1)-1;
         if(choice==movieList.size()) return;
         MovieList.setEndDate(movieList.get(x).getTitle());
     }
 
-    public void createCinemaShowtimes() {
-        ArrayList<Movie> movieList = MovieList.getMovieList();
-        for(int i=0;i<movieList.size();i++){
-            System.out.println(i+" " +movieList.get(i));
-        }   
-        System.out.println("Enter Movie Title to add");
-        String title=scan.nextLine();
-        if(MovieList.titleExists(title)){
+    public void createCinemaShowtimes(Cineplex cineplex) {
+        while(true){
+            ArrayList<Movie> movieList = cineplex.getListOfMovies();
+            System.out.println("Select movie to add ");
+            for(int i=0; i<movieList.size(); i++) System.out.println((i+1)+". "+movieList.get(i).getTitle());
+            System.out.println((movieList.size()+1) + ". Exit");
+            int choice = InputHandling.getInt("", "Invalid input: ", 1, movieList.size()+1)-1;
+            if(choice==movieList.size()) return;
+            String title = movieList.get(choice).getTitle();
+            System.out.println();
 
-        }
-        else{
-            String strInput;
-            System.out.println("Movie Not in Movie List");
-            System.out.println("Create new movie? Y/N");
-            do {
-                System.out.print("Invalid Input! Please input Y/N: ");
-                strInput= scan.nextLine();
-            } while (! ( (admin= strInput.charat(0).equalsIgnoreCase("y")) || strInput.charat(0).equalsIgnoreCase("n")) );
-            if(strInput.charat(0).equalsIgnoreCase("y")){
-                Movie newMovie = MovieList.createMovie(); //adds into master list
-                
-            }
-        }
-        
+            ArrayList<Cinema> cinemaList = cineplex.getCinemas();
+            int noCinemas = cineplex.getCinemas().size();
+            System.out.printf("Select cinema number (1-%d) \n", noCinemas);
+            choice = InputHandling.getInt("", "Invalid number: ", 1, noCinemas)-1;
+            if(choice==movieList.size()) continue;
+            System.out.println();
+
+            String screen = new SimpleDateFormat("HHmmyyyyMMdd").format(InputHandler.getDate());
+            cineplex.addScreening(choice, title, Integer.valueOf(screen.substring(0, 4)), Integer.valueOf(screen.substring(4))); 
+            System.out.println();
+        }       
     }
 
-    // public void updateCinemaListing() {
+    public void updateCinemaShowtimes(Cineplex cineplex) {
+        while(true){
+            ArrayList<Movie> movieList = cineplex.getListOfMovies();
+            System.out.println("Select movie to update ");
+            for(int i=0; i<movieList.size(); i++) System.out.println((i+1)+". "+movieList.get(i).getTitle());
+            System.out.println((movieList.size()+1) + ". Exit");
+            int choice = InputHandling.getInt("", "Invalid input: ", 1, movieList.size()+1)-1;
+            if(choice==movieList.size()) return;
+            String title = movieList.get(choice).getTitle();
+            System.out.println();
 
-    // }
+            ArrayList<Integer> screenList = cineplex.listOfScreeningByMovie(title);
+            System.out.println("Select screening to modify: ");
+            for(int i=0; i<movieList.size(); i++) System.out.println((i+1)+". "+
+            new SimpleDateFormat("dd MMM yyyy HH:mm").format(new SimpleDateFormat("yyyyMMddHHmm").parse(String.valueOf(screenList.get(i)))));
+            System.out.println((movieList.size()+1)+". Return");
+            choice = InputHandler.getInt("", "Invalid input: ", 1, movieList.size()+1)-1;
+            if(choice==movieList.size()) continue;
+            
+            String newScreen = new SimpleDateFormat("HHmmyyyyMMdd").format(InputHandler.getDate());
+            cineplex.updateScreeningShowtime(cineplex.cinemaFinder(title, screenList.get(choice)%1000, screenList.get(choice)/1000),
+            title, screenList.get(choice)%1000, Integer.valueOf(newScreen.substring(0,5)), screenList.get(choice)/1000, Integer.valueOf(newScreen.substring(5)));
+        }  
+    }
 
-    // public void deleteCinemaListing() {
+    public void addMovieToCineplex(Cineplex cineplex) {
+        while(true){
+            ArrayList<Movie> movieList = MovieList.getMovieList();
+            ArrayList<Movie> cineplexMovieList = cineplex.getListOfMovies();
+            ArrayList<String> validTitles = new ArrayList<String>();
+            for(Movie mov : movieList) if(!cineplexMovieList.contains(mov)) validTitles.add(mov.getTitle());
 
-    // }
+            System.out.println("Select movie to add: ");
+            for(int i=0; i<validTitles.size(); i++) System.out.println((i+1)+". "+validTitles.get(i));
+            System.out.println((validTitles.size()+1)+". Exit");
+            int choice = InputHandling.getInt("", "Invalid input: ", 1, movieList.size()+1)-1;
+            if(choice==validTitles.size()) return;
+
+            cineplex.addCineplexList(validTitles.get(choice));
+        }  
+
+    }
 
     // public void configureSystemSettings() {
     //     // ticket prices
