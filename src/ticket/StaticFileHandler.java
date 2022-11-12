13a1f2c4cdab05e0e2f36459d1dc7124faca0740
PockXuan <khorpockxuan@gmail.com>
2022-11-13 00:24:29 +0800

    bro

--- src/ticket/StaticFileHandler.java
+++ src/ticket/StaticFileHandler.java
@@ -1,6 +1,6 @@
 package ticket;
 
 public interface StaticFileHandler{
-    public void readFile();
-    public void writeFile();
+    public static void readFile();
+    public static void writeFile();
 }
