e3b1947f6248b240ee33e216a915b14f086554dd
Lucas Tan <65481995+sl33pingmathrapt0r@users.noreply.github.com>
2022-11-12 18:41:00 +0800

    new adminMain

---
+++ src/appMain.java
@@ -0,0 +1,9 @@
+package src;
+
+public class appMain {
+    // TO BE RENAMED AS Main
+    // CURRENT MAIN NEED TO BE SPLIT TO AdminMain AND GoerMain
+
+    //APPLICATION STARTUP
+    src.usr.Accounts.load();
+}
